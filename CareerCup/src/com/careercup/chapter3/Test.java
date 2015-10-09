2548b593c9a46b44d942ceab88fde9f97545d8a9
lunafeng <lunafeng123@gmail.com>
2015-10-09 15:39:37 -0300

    Set up git in eclipse

---
+++ CareerCup/src/com/careercup/chapter3/Test.java
@@ -0,0 +1,11 @@
+package com.careercup.chapter3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test git in eclipse");
+
+	}
+
+}
